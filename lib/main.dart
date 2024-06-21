7e0c69d647d21bca573fad8d8e2e6f66140d01a3
nadipallisudharshan <nadipallisudharshan@gmail.com>
2024-06-21 06:05:27 +0000

    FlutterMedia

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Images',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+
+
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ 
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: const Text("Flutter Images"),
+      ),
+    
+        body: ListView(
+          children: [
+            const Image(
+          image: AssetImage('lib/images/HTML.png')
+        ),
+
+            SizedBox(height: 20),
+          Text('HTML COURSE : 150000'),
+
+        const Image(
+          image: AssetImage('lib/images/css.png'),
+        ),
+        Text('CSS COURSE : 150000'),
+         SizedBox(height: 20),
+        const Image(
+          image: AssetImage('lib/images/Bs.png'),
+        ),
+         Text('BootStrap COURSE : 150000'),
+          SizedBox(height: 20),
+          ],
+        )
+
+
+        
+      
+      
+      
+         );
+  }
+}
